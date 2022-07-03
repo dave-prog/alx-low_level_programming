fbbe489c845577b266924f74b105d7ade03d6f36
Olanrewaju David <davidolanrewaju538@gmail.com>
2022-07-03 14:26:52 -0700

    Number combination in hundreds

---
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -0,0 +1,36 @@
+#include <stdio.h>
+
+/**
+ * main - Print possible digit combination in hundreds
+ *
+ * Return: Always 0 (Success)
+ *
+ */
+
+int main(void)
+{
+	int hundred;
+	int tens;
+	int unit;
+
+	for (hundred = 0; hundred < 8; hundred++)
+	{
+		for (tens = hundred + 1; tens < 9; tens++)
+		{
+			for (unit = tens + 1; unit <= 9; unit++)
+			{
+				putchar(hundred + '0');
+				putchar(tens + '0');
+				putchar(unit + '0');
+
+				if (hundred != 8)
+				{
+					putchar(',');
+					putchar(' ');
+				}
+			}
+		}
+	}
+	putchar('\n');
+	return (0);
+}
