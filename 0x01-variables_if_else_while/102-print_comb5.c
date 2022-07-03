73b866419ca61129a2929fde40b9636a1ae1c090
Olanrewaju David <davidolanrewaju538@gmail.com>
2022-07-03 16:14:57 -0700

    Combination of numbers

--- 0x01-variables_if_else_while/102-print_comb5.c
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -5,26 +5,26 @@
  */
 int main(void)
 {
-	int first_tens;
-	int first_unit;
+	int first_t;
+	int first_u;
 	int second_t;
 	int second_u;
 
-	for (first_tens = 0; first_tens < 9; first_tens++)
+	for (first_t = 0; first_t <= 9; first_t++)
 	{
-		for (first_unit = first_tens = 1; first_unit <= 9; first_unit++)
+		for (first_u = first_t = 1; first_u <= 9; first_u++)
 		{
-			for (second_t = 0; second_t < 9; second_t++)
+			for (second_t = first_t; second_t <= 9; second_t++)
 			{
-				for (second_u = first_unit + 1; second_u <= 9; second_u++)
+				for (second_u = first_u + 1; second_u <= 9; second_u++)
 				{
-					putchar(first_tens + '0');
-					putchar(first_unit + '0');
+					putchar(first_t + '0');
+					putchar(first_u + '0');
 					putchar(' ');
 					putchar(second_t + '0');
 					putchar(second_u + '0');
 
-					if (first_tens <= 9 || first_unit < 9 && second_t <= 9 || second_u <= 9)
+					if ((first_t <= 9 && first_u < 9) && (second_t <= 9 && second_u <= 9))
 					{
 						putchar(',');
 						putchar(' ');
