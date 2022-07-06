a504731819db35da17826f6b191995c0060464eb
Olanrewaju David <davidolanrewaju538@gmail.com>
2022-07-06 03:26:02 -0700

    Alphabet Game

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,25 @@
+#include "main.h"
+
+
+/**
+ * print_alphabet - writes out the alphabet in lower case
+ *
+ *Return: void.
+ */
+
+
+void print_alphabet(void)
+{
+	char alpha;
+
+	alpha = 'a';
+
+	while (alpha <= 'z')
+	{
+		_putchar(alpha);
+
+		alpha++;
+	}
+
+	_putchar('\n');
+}
--- 0x02-functions_nested_loops/README.md
+++ 0x02-functions_nested_loops/README.md
@@ -2,3 +2,6 @@
 
 ## 0. _putchar
 > Program that prints _putchar, followed by a new line.
+
+## 1. Alphabet Game
+> A function that prints the alphabet, in lowercase, followed by a new line.
