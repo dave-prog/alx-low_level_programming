a8279aa18862176d5d8c85dc8160e8d770581bd4
Olanrewaju David <davidolanrewaju538@gmail.com>
2022-07-12 13:41:15 -0700

    Dereferencing

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,13 @@
+#include "main.h"
+
+/**
+ * reset_to_98 - set the integer to 98
+ * @n: a pointer integer we want to set to 98
+ *
+ * Return: nothing
+ */
+
+void reset_to_98(int *n)
+{
+	*n = 98;
+}
