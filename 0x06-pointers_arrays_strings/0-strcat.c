ee6fabdaadab38db9a02947d576248416528b9d6
Olanrewaju David <davidolanrewaju538@gmail.com>
2022-07-15 01:21:29 -0700

    String Concatenation

--- 0x06-pointers_arrays_strings/0-strcat.c
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -14,17 +14,18 @@ char *_strcat(char *dest, char *src)
 	int i = 0;
 	int j = 0;
 
-	while (src[j] != '\0')
-	{
-		j++;
-	}
-
 	while (dest[i] != '\0')
 	{
 		i++;
+	}
+
+	while (src[j] != '\0')
+	{
+		dest [i] = src[j];
+		i++;
 		j++;
 	}
-	dest = '\0';
+	dest[i] = '\0';
 
 	return (dest);
 }
